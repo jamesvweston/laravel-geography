084983ed6eb4b3a154dcd2dd98611f5989413243
james weston <jweston@fulfillment.com>
2016-02-14 21:55:58 -0500

    Adding getById function

--- src/Repositories/Geography/AddressRepository.php
+++ src/Repositories/Geography/AddressRepository.php
@@ -2,7 +2,22 @@
 namespace app\Repositories\Geography;
 
 
+use app\Models\Geography\Address;
+use LaravelDoctrine\ORM\Pagination\Paginatable;
+use Doctrine\ORM\Query;
+
 class AddressRepository extends BaseGeographyRepository
 {
 
+    use Paginatable;
+
+    /**
+     * Get a single Address object by its id
+     * @param       int                 $id                 Id to query against
+     * @return      Address|null
+     */
+    public function getOneById($id)
+    {
+        return $this->find($id);
+    }
 }
